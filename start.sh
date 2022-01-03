cd3f9084b1f041be86233c223a7d3d909720b5c4
cyx9990 <94532537+cyx9990@users.noreply.github.com>
2022-01-03 19:02:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,8 +15,4 @@ for file in $(ls /usr/share/caddy/$AUUID); do
 done
 
 # start
-tor &
-
-/xray -config /xray.json &
-
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
+sudo cat /etc/caddy/Caddyfile
